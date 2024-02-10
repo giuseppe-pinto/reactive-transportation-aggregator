cf42c3db5d4e9e5109b824c7399152e41a9eed1c
Giuseppe Pinto <giuseppe.pinto@lastminute.com>
2024-02-10 15:48:49 +0100

    now we call the supplier in parallel on a different threads

--- src/main/java/giuseppe/pinto/transportation/aggregator/adapter/secondary/in/StandardDriverOutcomeService.java
+++ src/main/java/giuseppe/pinto/transportation/aggregator/adapter/secondary/in/StandardDriverOutcomeService.java
@@ -3,13 +3,9 @@ package giuseppe.pinto.transportation.aggregator.adapter.secondary.in;
 import giuseppe.pinto.transportation.aggregator.domain.DriverOutcome;
 import giuseppe.pinto.transportation.aggregator.domain.OneWaySearchRequest;
 import giuseppe.pinto.transportation.aggregator.port.in.DriverOutcomeService;
-import giuseppe.pinto.transportation.aggregator.port.out.*;
-import giuseppe.pinto.transportation.aggregator.port.out.driver.DriverRepository;
+import giuseppe.pinto.transportation.aggregator.port.out.DriverConfigurationRepository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import reactor.core.scheduler.Schedulers;
 
 public class StandardDriverOutcomeService implements DriverOutcomeService {
 
@@ -22,16 +18,12 @@ public class StandardDriverOutcomeService implements DriverOutcomeService {
     @Override
     public Flux<DriverOutcome> from(OneWaySearchRequest oneWaySearchRequest) {
 
-        Flux<DriverRepository> drivers = driverConfigurationRepository
-                .getDriversFor(oneWaySearchRequest);
-
-        return Flux.merge(drivers.map(driverRepository -> driverRepository.performRequest(oneWaySearchRequest)));
-
-
-        /*return Flux.merge( drivers
-                    .stream()
-                    .map(driver -> driver.performRequest(oneWaySearchRequest))
-                    .collect(Collectors.toList()));*/
+        return driverConfigurationRepository
+                .getDriversFor(oneWaySearchRequest)
+                .parallel()
+                .runOn(Schedulers.parallel())
+                .flatMap(driverRepository -> driverRepository.performRequest(oneWaySearchRequest))
+                .sequential();
 
     }
 
